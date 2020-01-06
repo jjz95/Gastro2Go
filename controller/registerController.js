2056c882bbb36bc94ea311ec6b1e0003c179b832
MK <m0kotlicki@gmail.com>
2020-01-06 21:04:21 +0100

    user registration  works

--- controller/registerController.js
+++ controller/registerController.js
@@ -6,14 +6,48 @@ const User = require('../model/user');
 router.get("/", (req, res, next) => {
     res.render('rejestracja')
 })
+//firstName, lastName, email, passwordHash, dateOfBirth, contactNumber, business, address, zipCode, country
+router.post("/", async (req, res, next) => {
+    const dateReg = /^\d{4}[\/\-]((0?[1-9]|1[012])|[a-z]+)[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
+    const emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const lettersReg = /^([a-zA-Z]{2,})$/;
+    var numberReg = /^\d+$/;
+    if (dateReg.test(req.body.fdate)) {
+        let imie = req.body.fimie
+        let nazwisko = req.body.fnazwisko
+        let email = req.body.femail
+        let pass = req.body.fpass
+        let pass2 = req.body.fpass2
+        let date = new Date(req.body.fdate)
+        let numer = parseInt(req.body.fnumer)
+        let dzialalnosc = req.body.fdzialalnosc
+        let adres = req.body.fadres
+        let kod = req.body.fkod
+        let kraj = req.body.fkraj
 
-router.post("/", (req, res, next) => {
-  User.hashPassword(req.body.password)
-    .then(hash => {
-      const newUser = new User(req.body.first_name, req.body.last_name, req.body.email, hash);
-      User.add(newUser);
-      res.redirect("/users");
-    }); 
+        console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+
+        if (pass === pass2 &&
+            lettersReg.test(imie) &&
+            lettersReg.test(nazwisko) &&
+            emailReg.test(email) &&
+            pass !== '' &&
+            numberReg.test(numer) &&
+            lettersReg.test(dzialalnosc) &&
+            adres !== '' &&
+            kod !== '' &&
+            lettersReg.test(kraj)
+        ) {
+
+            const newUser = new User(imie, nazwisko, email, pass, date, numer, dzialalnosc, adres, kod, kraj);
+            await User.add(newUser);
+
+        } else {
+            console.log('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+        }
+        console.log(User.list())
+        res.redirect("/");
+    }
 });
 
 module.exports.route = router; 
--- model/user.ts
+++ model/user.ts
@@ -6,8 +6,19 @@ let nextId = 1;
 const userExtent = [];
 
 class User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    passwordHash: string;
+    dateOfBirth: Date;
+    contactNumber: number;
+    business: string;
+    address: string;
+    zipCode: string;
+    country: string;
     //parametr id jest na końcu, bo jest opcjonalny
-    constructor(firstName, lastName, email, passwordHash, dateOfBirth, contactNumber, business, address, zipCode, country, id) {
+    constructor(firstName: string, lastName: string, email: string, passwordHash: string, dateOfBirth: Date, contactNumber: number, business: string, address: string, zipCode: string, country: string, id: number) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -22,7 +33,7 @@ class User {
     }
 
     //dodawanie obiektu do bazy
-    static async add(user) {
+    static async add(user: User) {
         // user.id = nextId++;
         // userExtent.push(user);
         // return user;
@@ -45,47 +56,51 @@ class User {
     }
 
     //edycja obiektu
-    static edit(user) {
+    static edit(user: any) {
         //FIXME
     }
 
     //usuwanie obiektu po id
-    static delete(id) {
+    static delete(id: any) {
         //FIXME
     }
 
     //pobieranie obiektu do widoku szczegółów
     //może być potrzebne pobranie dodatkowych danych
     //np. przez złączenia JOIN w relacyjnej bazie danych
-    static details(id) {
+    static details(id: any) {
         //FIXME
     }
 
     //metoda resetuje stan bazy i dodaje rekordy testowe
     //przydatna do testów
-    static async initData() {
+    static initData() {
         //usuwamy zawartość tablicy
         userExtent.splice(0, userExtent.length);
         //resetujemy licznik id
         nextId = 1;
 
         //dla uproszczenia wszyscy użytkowinicy mają takie samo hasło :)
+
+        // @ts-ignore
         User.add(new User('Jan', 'Kowalski', 'jk@wp.pl', '1234', new Date("1990-03-25"), 123456789, 'fishing', 'koszykowa', '00-001', 'Poland'));
+        // @ts-ignore
         User.add(new User('Anna', 'Wiśniewska', 'aw@onet.pl', '1234', new Date("1991-03-25"), 111111111, 'skiing', 'hitler strasse', '00-003', 'Germany'));
+        // @ts-ignore
         User.add(new User('Andrzej', 'Nowak', 'an@gmail.com', '1234', new Date("1992-03-25"), 222222222, 'snowboard', 'smith street', '00-002', 'England'));
     }
 
-    static findByEmail(email) {
+    static findByEmail(email: any) {
         return userExtent.find(u => u.email == email);
     }
 
-    static hashPassword(plainPassword) {
+    static hashPassword(plainPassword: any) {
         //wołanie asynchroniczne
         //zwraca promesę, a nie wynik bezpośrednio
         return bcrypt.hash(plainPassword, 12);
     }
 
-    comparePassword(plainPassword) {
+    comparePassword(plainPassword: any) {
         //wołanie asynchroniczne
         //zwraca promesę, a nie wynik bezpośrednio
         return bcrypt.compare(plainPassword, this.passwordHash);
